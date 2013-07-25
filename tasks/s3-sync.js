4724828618f112f28900d4be854ee17eba5e7082
François Robichet <calvein@gmail.com>
2013-07-25 11:56:37 +1000

    Change the gzipping

--- tasks/s3-sync.js
+++ tasks/s3-sync.js
@@ -25,9 +25,10 @@ module.exports = function(grunt) {
       options = false
     }
 
-    // Init the stream
+    // // Init the stream
     var stream = s3sync(db, options)
 
+    // Log upload message
     stream.on('data', function(file) {
       grunt.log.ok(file.absolute, file.relative)
     })
@@ -35,30 +36,47 @@ module.exports = function(grunt) {
       grunt.log.error(err.toString())
     })
 
+    // Get the actual number of files to upload
+    var nbFiles = grunt.util._.chain(this.files)
+    .pluck('src')
+    .pluck('length')
+    .reduce(function(a, b) {
+        return a + b
+    })
+    .value()
+
+    // Handle the upload for each files
+    var uploadFile = function(src, dest) {
+        stream.write({
+            src: src
+          , dest: dest
+        })
+        // When all the files are uploaded
+        if (--nbFiles === 0) {
+          fs.rmdirSync('.tmp')
+          stream.end()
+        }
+
+    }
+
+    // Upload each file
     this.files.forEach(function(file) {
       file.src.forEach(function(src) {
         // GZip the file
         if (file.gzip) {
-          return fs.readFile(src, function(err, data) {
-            var buffer = new Buffer(data)
-            zlib.gzip(buffer, function(err, data) {
-              if (!err) {
-                stream.write({
-                    src: data
-                  , dest: file.dest
-                })
-              }
-            })
-          })
-        }
+          var gzip = zlib.createGzip()
+            , input = fs.createReadStream(src)
+            , outputSrc = '.tmp/' + src
+            , output = fs.createWriteStream(outputSrc)
 
-        stream.write({
-            src: src
-          , dest: file.dest
-        })
+          input.pipe(gzip).pipe(output)
+          output.on('close', function() {
+            uploadFile(src, file.dest)
+          })
+        } else {
+          uploadFile(src, file.dest)
+        }
       })
     })
-
-    stream.end()
   })
 }
